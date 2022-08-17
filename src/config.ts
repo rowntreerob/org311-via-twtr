29722596309d4c72b5afa673aadc83007e61496f
Rob Rowntree <rowntreerob@gmail.com>
2022-08-16 18:12:47 -0700

    process.env bug only in prod fixed w bettr impl of dotenv

--- src/config.ts
+++ src/config.ts
@@ -1,21 +1,15 @@
 import fs from 'fs';
 import dotenv from 'dotenv';
 import { TwitterApi } from 'twitter-api-v2';
-
-
-
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-export const CONFIG = dotenv.parse(fs.readFileSync(__dirname + '/../.env'));
-
+import 'dotenv/config';
+// export const CONFIG = dotenv.parse(fs.readFileSync(__dirname + '/../.env'));
+let CONFIG = {};
 export const TOKENS = {
-  appKey: CONFIG.CONSUMER_TOKEN,
-  appSecret: CONFIG.CONSUMER_SECRET,
+  appKey: process.env.CONSUMER_TOKEN!,
+  appSecret: process.env.CONSUMER_SECRET!,
 };
-
 // Create client used to generate auth links only
 export const requestClient = new TwitterApi({ ...TOKENS });
-
-export default CONFIG;
+export default CONFIG ={PORT: process.env.PORT!};
