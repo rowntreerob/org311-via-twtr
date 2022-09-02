d8b26e1f93133270b1616193716c89bc87541fd8
Rob Rowntree <rowntreerob@gmail.com>
2022-09-01 17:51:38 -0700

    twitter css on the authorize page. using signed commit newby

--- src/config.ts
+++ src/config.ts
@@ -4,9 +4,10 @@ import { TwitterApi } from 'twitter-api-v2';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import 'dotenv/config';
+// NODE_ENV, PORT, CONSUMER_TOKEN, CONSUMER_SECRET
+// GOOGLE_MAPS_API_KEY, MAPS_API_DOMAIN, OAUTH_DOMAIN
 // export const CONFIG = dotenv.parse(fs.readFileSync(__dirname + '/../.env'));
 let MAP = {KEY: process.env.GOOGLE_MAPS_API_KEY!, GEOCD: process.env.MAPS_API_DOMAIN!}
-
 let CONFIG = {};
 export const TOKENS = {
   appKey: process.env.CONSUMER_TOKEN!,
