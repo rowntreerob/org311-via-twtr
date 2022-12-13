477d07cdad6bcb6d8c1381f64f60e89167c0ccbe
rob <rowntreerob@gmail.com>
2022-12-13 10:53:33 -0800

    rm heroku ins railway mod ejs to receive just ID add 2 separate clients for v1, v2 api calls , update ver on twitter-api-v2

--- src/routes/callback.ts
+++ src/routes/callback.ts
@@ -5,6 +5,7 @@ import CONFIG, { requestClient, TOKENS } from '../config.js';
 import { asyncWrapOrError } from '../utils.js';
 import  got  from 'got';
 import exifr from 'exifr';
+import mime from'mime-types';
 
 export const callbackRouter = Router();
 
@@ -15,7 +16,8 @@ export const callbackRouter = Router();
 callbackRouter.get('/', asyncWrapOrError(async (req, res) => {
   let link;
   if (process.env.NODE_ENV === 'development') {
-    link = await requestClient.generateAuthLink(`http://localhost:${CONFIG.PORT}/callback`);
+     link = await requestClient.generateAuthLink(`http://localhost:${CONFIG.PORT}/callback`);
+	 // link = await requestClient.generateAuthLink(`http://localhost:${CONFIG.PORT}/callback/photo`);
   } else {
     link = await requestClient.generateAuthLink(`${CONFIG.OAUTH_DOMAIN}callback`);
   }
@@ -27,13 +29,15 @@ callbackRouter.get('/', asyncWrapOrError(async (req, res) => {
 /*
 session.store url from aws bucket request from diff application
 for auth service, access_token and twitter api *post.status* with media_id
+route belo called by bubble front end in WF for button.upload
 */
 callbackRouter.get('/photo/:url/tag/:hashtg', asyncWrapOrError(async (req, res) => {
+  
   let link;
   if (process.env.NODE_ENV === 'development') {
     link = await requestClient.generateAuthLink(`http://localhost:${CONFIG.PORT}/callbk`);
   } else {
-    link = await requestClient.generateAuthLink(`https://pure-dusk-35729.herokuapp.com/callbk`);
+    link = await requestClient.generateAuthLink(`${CONFIG.OAUTH_DOMAIN}callbk`);
   }
 
   // Save token secret to use it after callback
@@ -47,7 +51,6 @@ callbackRouter.get('/photo/:url/tag/:hashtg', asyncWrapOrError(async (req, res) 
 
   callbackRouter.get('/callback', asyncWrapOrError(async (req, res) => {
     // Invalid request
-
     if (!req.query.oauth_token || !req.query.oauth_verifier) {
       res.status(400).render('error', { error: 'Bad request, or you denied application access. Please renew your request.' });
       return;
@@ -64,7 +67,8 @@ callbackRouter.get('/photo/:url/tag/:hashtg', asyncWrapOrError(async (req, res) 
     const tempClient = new TwitterApi({ ...TOKENS, accessToken: token, accessSecret: savedSecret });
     // Ask for definitive access token
     const { accessToken, accessSecret, client, screenName, userId } = await tempClient.login(verifier);
-    const homeTimeline = await client.v1.tweet('testing sts api');
+	 console.log('client logon exit')
+    const homeTimeline = await client.v2.tweet('testing sts api');
     res.render('callback', { accessToken, accessSecret, screenName, userId });
   }));
 
@@ -81,7 +85,9 @@ callbackRouter.get('/callbk', asyncWrapOrError(async (req, res) => {
   const savedSecret = req.session.oauthSecret;
   const savedUrl = req.session.photoUrl;
   const savedTag = req.session.hashTag;
-  // console.log(savedUrl);
+  const myArray = savedUrl.split(".");
+  const val =  myArray[myArray.length - 1]   
+  const mimeTyp = mime.lookup(val)
   if (!savedToken || !savedSecret || savedToken !== token) {
     res.status(400).render('error', { error: 'OAuth token is not known or invalid. Your request may have expire. Please renew the auth process.' });
     return;
@@ -90,30 +96,30 @@ callbackRouter.get('/callbk', asyncWrapOrError(async (req, res) => {
   const tempClient = new TwitterApi({ ...TOKENS, accessToken: token, accessSecret: savedSecret });
   // Ask for definitive access token
   const {  accessToken, accessSecret, client, screenName, userId } = await tempClient.login(verifier);
-  // You can store & use accessToken + accessSecret to create a new client and make API calls!
+  const v1Client = client.v1;
+  const v2Client = client.v2;
   const myBuff = await got({ url: savedUrl }).buffer(); //fetch photo as buffer from AWS bucket
   let {latitude, longitude} = await exifr.gps(myBuff);  // api -> get EXIF latlng from buffer(photo)
   // api from gps.latLng to street address
   const rsult = await got.get(
   `${CONFIG.MAP.GEOCD}?latlng=${latitude},${longitude}&key=${CONFIG.MAP.KEY}`).json();
   const mapAddr = rsult.results[0].formatted_address;  // parse street.addr
-  const myId :string = await client.v1.uploadMedia(myBuff,  { type: "png" }); // supply photo to twitr API
-  const mytweet = await client.v1.tweet(`@yayatvapp  reporting streets issue via picture  ${mapAddr} #${savedTag} ` , {media_ids: myId}); // submit tweet w media -> photo
-  const short: string = mytweet.full_text;
-  //console.log(short)
-  const stsLnk = parseLnk(short, '://');
-  console.log(mytweet.full_text, ' ', stsLnk)
-  res.render('callback', {  latitude, longitude, screenName, stsLnk });
+  const myId :string = await v1Client.uploadMedia(myBuff,  { mimeType: mimeTyp }); // supply photo to twitr API
+  const data = await 
+    v2Client.tweet(`@yayatvapp  reporting streets issue via picture  ${mapAddr} #${savedTag} ` , {media: {media_ids: [myId]}});
+  const statusId  = data.data.id; 
+  res.render('callback', {  latitude, longitude, screenName, statusId});
 }));
 
 // Read data from Twitter callback
-callbackRouter.get('/callback/:photo', asyncWrapOrError(async (req, res) => {
+callbackRouter.get('/callback/photo', asyncWrapOrError(async (req, res) => {
   // Invalid request
   if (!req.query.oauth_token || !req.query.oauth_verifier) {
     res.status(400).render('error', { error: 'Bad request, or you denied application access. Please renew your request.' });
     return;
   }
-const _url = decodeURI(req.params.photo);
+  const _url = 'https://bubble-yaya-tst.s3.amazonaws.com/photo/-4n8Kx9DXgupJtQ5G-i9C/fred_fwrd_5yr.png'
+//const _url = decodeURI(req.params.photo);
 
   const token = req.query.oauth_token as string;
   const verifier = req.query.oauth_verifier as string;
@@ -124,12 +130,14 @@ const _url = decodeURI(req.params.photo);
   const tempClient = new TwitterApi({ ...TOKENS, accessToken: token, accessSecret: savedSecret });
   // Ask for definitive access token
   const {  accessToken, accessSecret, client, screenName, userId } = await tempClient.login(verifier);
-  // You can store & use accessToken + accessSecret to create a new client and make API calls!
-  //const _url = 'https://bubble-yaya-tst.s3.amazonaws.com/photo/-4n8Kx9DXgupJtQ5G-i9C/fred_fwrd_5yr.png';
+const v1Client = client.v1;
+const v2Client = client.v2;
+  
   const myBuff = await got({ url: _url }).buffer();
-  const myId :string = await client.v1.uploadMedia(myBuff,  { type: "png" });
-  await client.v1.tweet('@yayatvapp testing streets reporting via picture  #savethedrop' , {media_ids: myId});
-
+  const myId :string = await v1Client.uploadMedia(myBuff,  { type: "png" });
+  
+  await v2Client.tweet(' testing streets reporting via picture  #savethedrop' , {media: {media_ids: [myId]}});
+  
   res.render('callback', { accessToken, accessSecret, screenName, userId });
 }));
 
